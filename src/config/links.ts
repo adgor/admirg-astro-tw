da51e5bd9e653b0a1be5407b50f1f1137c1efa3f
adgor <adgor000@gmail.com>
2022-08-21 00:17:36 +0200

    initial cmm

---
+++ src/config/links.ts
@@ -0,0 +1,46 @@
+export const navLinks = [
+  {
+    "title": "official website",
+    "url": "/"
+  },
+  {
+    "title": "portfolio",
+    "url": "/portfolio"
+  },
+  {
+    "title": "blog",
+    "url": "/blog"
+  },
+  {
+    "title": "about",
+    "url": "/about"
+  },
+  {
+    "title": "/links",
+    "url": "/links"
+  },
+  {
+    "title": "/uses",
+    "url": "/uses"
+  },
+]
+
+
+  export const socialLinks = [
+    {
+        "url": "https://instagram.com/admirgorenca",
+        "description": "Admir Gorenca on Instagram",
+        "icon": "mdi:instagram"
+    },
+    {
+        "url": "https://twitter.com/admirgorenca",
+        "description": "Admir Gorenca on Twitter",
+        "icon": "mdi:twitter"
+    },
+    {
+        "url": "https://github.com/admirgorenca",
+        "description": "Admir Gorenca on GitHub",
+        "icon": "mdi:github"
+    }
+]
+
