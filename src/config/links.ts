c31cf7e4485b9c6ed6121e30d1e80cf8a850c2ef
adgor <adgor000@gmail.com>
2022-09-02 02:28:55 +0200

    add about sec

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,46 +1,46 @@
 export const navLinks = [
   {
-    "title": "portfolio",
-    "url": "/portfolio"
+    title: "portfolio",
+    url: "/portfolio",
   },
   {
-    "title": "blog",
-    "url": "/blog"
+    title: "blog",
+    url: "/blog",
   },
   {
-    "title": "about",
-    "url": "/about"
+    title: "about",
+    url: "/#about",
   },
   {
-    "title": "/links",
-    "url": "/links"
+    title: "/links",
+    url: "/links",
   },
   {
-    "title": "/uses",
-    "url": "/uses"
+    title: "/uses",
+    url: "/uses",
   },
-]
+];
 
-  export const socialLinks = [
-    {
-        "url": "https://instagram.com/admirgorenca",
-        "description": "Admir Gorenca on Instagram",
-        "icon": "mdi:instagram"
-    },
-    {
-        "url": "https://twitter.com/admirgorenca",
-        "description": "Admir Gorenca on Twitter",
-        "icon": "mdi:twitter"
-    },
-    // {
-    //     "url": "https://github.com/admirgorenca",
-    //     "description": "Admir Gorenca on GitHub",
-    //     "icon": "mdi:github"
-    // },
-    // {
-    //     "url": "https://discord.gg/93PJFCUP4k",
-        
-    //     "description": "Admir Gorenca on Discord",
-    //     "icon": "mdi:discord"
-    // },
-]
+export const socialLinks = [
+  {
+    url: "https://instagram.com/admirgorenca",
+    description: "Admir Gorenca on Instagram",
+    icon: "mdi:instagram",
+  },
+  {
+    url: "https://twitter.com/admirgorenca",
+    description: "Admir Gorenca on Twitter",
+    icon: "mdi:twitter",
+  },
+  // {
+  //     "url": "https://github.com/admirgorenca",
+  //     "description": "Admir Gorenca on GitHub",
+  //     "icon": "mdi:github"
+  // },
+  // {
+  //     "url": "https://discord.gg/93PJFCUP4k",
+
+  //     "description": "Admir Gorenca on Discord",
+  //     "icon": "mdi:discord"
+  // },
+];
