cceebc9b0be76bffe63c1e723055a80d5bfb92dc
adgor <adgor000@gmail.com>
2023-04-30 19:09:56 +0200

    add clients Pg

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 // global data
 
-export const NAME = 'Admir Gorenca';
-export const SITE_TITLE = 'Admir Gorenca - Web Developer';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const NAME = "Admir Gorenca";
+export const SITE_TITLE = "Admir Gorenca - Web Developer";
+export const SITE_DESCRIPTION = "Welcome to my website!";
